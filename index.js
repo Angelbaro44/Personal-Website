cd71f34fdb74cd2a95065cc1c364ca2f362fa213
Angel Baro <angelbaro44@gmail.com>
2020-12-30 14:46:48 -0500

    save

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let dynamicGradient= ()=>{
+  
+      let linearGradient = 'linear-gradient(60deg, #3d3393 0%, #2b76b9 37%, #2cacd1 65%, #35eb93 100%)'
+
+
+      document.querySelector("#gradient").style.backgroundImage = linearGradient;
+
+
+}
